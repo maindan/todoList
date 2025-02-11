c02b14509e131ae841142446e59fc7a8acbffff9
Dan <danielkksilva1@gmail.com>
2025-02-11 01:22:20 -0400

    feat: add json config in app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const router = require('./routes/routes');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(router);
 
 const port = process.env.PORT;
 
